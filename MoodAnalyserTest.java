2d9eba8a0a891b4651cad40344c997fb31fcb2a9
pranav21999 <pranavdani99@gmail.com>
2021-07-09 23:31:11 +0530

    Added:TestCase For Happy condition

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -4,7 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
+	
+	//(uc1.1)return sad 
 	@Test
 	public void givenMessage_whenSad_ShouldReturnSad() {
 		// MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
@@ -23,4 +24,15 @@ public class MoodAnalyserTest {
 		String expected = "SAD";
 		Assert.assertEquals(expected, actual);
 	}
+	
+	//(uc1.2)return  happy 
+	@Test
+	public void givenMessage_whenHappy_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
+		String actual = moodAnalyzer.analyseMood();
+		String expected = "HAPPY";
+		Assert.assertEquals(expected, actual);
+	}
+
+
 
